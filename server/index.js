54b5f39070dfe27605bc4e39bd3a4fc924467b63
Mitish Raina <rainamitish7@gmail.com>
2025-03-12 01:10:16 +0530

    ArtSquirt

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './mongodb/connect.js';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit:'50mb' }));
+
+app.get('/', async(req, res) => {
+    res.send("Hello from ArtSquirt");
+})
+
+const startServer = async() => {
+    try {
+        connectDB(process.env.MONGODB_URL);
+        app.listen(3000, () => console.log("server is running on port http://localhost:3000"));
+    } catch (error) {
+        console.log(error);
+    }
+}
+startServer();
