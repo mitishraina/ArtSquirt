85c8f1d051e895740737b6adc10fe63e5c21708f
Mitish Raina <rainamitish7@gmail.com>
2025-03-13 02:54:47 +0530

    final update

--- server/routes/artSquirtRoutes.js
+++ server/routes/artSquirtRoutes.js
@@ -20,13 +20,14 @@ router.route('/').get((req, res) => {
 router.route('/').post( async(req, res) => {
     try {
         const { prompt } = req.body;
-        const aiResponse = await openai.createImage({
+        const aiResponse = await openai.images.generate({
+            // model: 'gpt-3.5-turbo-0613o',
             prompt,
             n: 1,
             size: "1024x1024",
             response_format: "b64_json"
         });
-        const image = aiResponse.data.data[0].b64_json;
+        const image = aiResponse.data[0].b64_json;
         res.status(200).json({ photo: image });
     } catch (error) {
         console.log(error);
