05c7802c329787710c5ede62518b0aa5be464ceb
Mitish Raina <rainamitish7@gmail.com>
2025-03-12 19:46:31 +0530

    image generator done

--- server/index.js
+++ server/index.js
@@ -2,12 +2,17 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/PostRoutes.js';
+import artSquirtRoutes from './routes/artSquirtRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit:'50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/artSquirt', artSquirtRoutes);
 
 app.get('/', async(req, res) => {
     res.send("Hello from ArtSquirt");
